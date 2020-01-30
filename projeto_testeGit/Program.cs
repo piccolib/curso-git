e1f5b4ab9c606552f311dcbd6a4c407b79949e40
bpiccoli <brunoxp99@gmail.com>
2020-01-29 23:53:39 -0300

    Acrescentada mensagem de boa noite

--- projeto_testeGit/Program.cs
+++ projeto_testeGit/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_testeGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Boa noite!");
         }
     }
 }
